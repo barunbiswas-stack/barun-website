b99372b1e951e04c9aaf05fe415777674d5b5457
barunbiswas-stack <barunbiswas0777@gmail.com>
2026-07-26 11:20:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+// ===============================
+// Learn More Button Popup
+// ===============================
+
+
+let button = document.getElementById("learnBtn");
+
+let popup = document.getElementById("popup");
+
+let closeBtn = document.getElementById("closePopup");
+
+
+if(button && popup){
+
+    button.addEventListener("click", function(){
+
+        popup.style.display = "flex";
+
+    });
+
+}
+
+
+if(closeBtn && popup){
+
+    closeBtn.addEventListener("click", function(){
+
+        popup.style.display = "none";
+
+    });
+
+}
+
+
+
+
+// ===============================
+// Mobile Menu
+// ===============================
+
+
+let menuBtn = document.getElementById("menuBtn");
+
+let navMenu = document.getElementById("navMenu");
+
+
+if(menuBtn && navMenu){
+
+    menuBtn.addEventListener("click", function(){
+
+        navMenu.classList.toggle("active");
+
+    });
+
+}
+
+
+
+
+
+// ===================================
+// Scroll Animation
+// ===================================
+
+
+let reveals = document.querySelectorAll(".reveal");
+
+
+window.addEventListener("scroll", function(){
+
+
+    for(let i = 0; i < reveals.length; i++){
+
+
+        let windowHeight = window.innerHeight;
+
+
+        let elementTop = reveals[i].getBoundingClientRect().top;
+
+
+        let elementVisible = 100;
+
+
+
+        if(elementTop < windowHeight - elementVisible){
+
+
+            reveals[i].classList.add("active");
+
+
+        }
+
+
+    }
+
+
+});
+
+
